34130a2a1d58d2b3bdd47e0909464ce2f96f05e5
ShaShu12138 <Zejing2001@hotmail.com>
2023-06-17 16:27:22 +0200

    update leverancier page

--- Pages/leverancier.php
+++ Pages/leverancier.php
@@ -11,6 +11,31 @@
         }
         .deliver-time > p,input{
             display: inline;
+            font-size:20px;
+        }
+        .deliver-time{
+            width: 400px;
+            display: block;
+            margin:50px auto;
+        }
+        .deliver-buttons{
+            width: 500px;
+            display: flex;
+            margin:50px auto;
+            justify-content : space-between;
+        }
+        .Add-button, .Delete-button{
+            width:150px;
+            height:30px;
+        }
+        .Upload-button{
+            width:150px;
+            height:50px;
+            font-size:20px;
+            position: fixed;
+            top:80%;
+            left: 50%;
+            transform: translate(-50%,100%);
         }
     </style>
 </head>
@@ -20,8 +45,34 @@
     </div>
     <div class="deliver-time">
         <p>Time of next deliver: </p>
-        <input type="date" name="party" min="2023-06-01" max="2017-04-30" />
+        <input type="date" name="deliver-date" value='<?= date('Y-m-d');?>' min="<?= date('Y-m-d');?>" />
     </div>
-    <p><?php echo date('Y-m-d');?></p>
+    <div id="product-list">
+
+    </div>
+    <div class="deliver-buttons">
+        <button class="Add-button" onclick="AddProject()">+ Add a product</button>
+        <button class="Delete-button" onclick="DeleteProject()">- Delete a product</button>
+    </div>
+    <button class="Upload-button">Submit</button>
 </body>
+<script>
+    function AddProject(){
+        //A big div
+        let div = document.createElement("div");
+        let place = document.querySelector("#product-list");
+        place.appendChild(div);
+        let divEl = document.querySelector("#product-list").lastChild;
+        divEl.classList.add("product");
+        //Product Name
+        let span = document.createElement("span");
+        let t = document.createTextNode("Product Name");
+        span.appendChild(t);
+        divEl.appendChild(span);
+        //Product Name input
+        let input = document.createElement("input");
+        divEl.appendChild(input);
+
+    }
+</script>
 </html>
