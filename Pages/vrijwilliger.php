fd30f8d9de2156c808b58ac6d31b4c6c4b3f1e14
ShaShu12138 <Zejing2001@hotmail.com>
2023-06-19 10:30:09 +0200

    Update vrijwilliger.php

--- Pages/vrijwilliger.php
+++ Pages/vrijwilliger.php
@@ -56,6 +56,7 @@
             width:70px;
         }
     </style>
+    <script src="https://cdn.jsdelivr.net/npm/js-cookie@3.0.1/dist/js.cookie.min.js"></script>
 </head>
 <body>
 <?php
@@ -75,28 +76,96 @@
     $ClientResult = mysqli_query($conn, $ClientSQL);
     $WarehouseSQL = "SELECT idDeliveredProduct, companyName, productName, productType, productEAN, productQuantity, productShelfLife FROM warehouseinventory WHERE productQuantity > 0 AND productShelfLife > CURDATE() ORDER BY productShelfLife";
     $WarehouseResult = mysqli_query($conn, $WarehouseSQL);
-    $PackageSQL = "SELECT * FROM packageproduct" ;
+    if($_COOKIE["PackageClient"] || $_COOKIE["PackageDate"]){
+    $CookieClient = $_COOKIE["PackageClient"];
+    $CookieDate = $_COOKIE["PackageDate"];
+    }else{
+    $CookieClient = "0";
+    $CookieDate = "0";
+    }
+    $PackageSQL = "SELECT * FROM packageproduct WHERE idClient = '$CookieClient' AND pickupTime = '$CookieDate'" ;
     $PackageResult = mysqli_query($conn, $PackageSQL);
 
+    //Package按钮
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["packageID"])) {
+        // 获取所需的数据
+        $DeliveredProductID = $_POST["packageID"];
+        $packageInput = $_POST["packageInput"];
+
+        //更新仓库数据
+        $WarehouseSQL = "SELECT companyName, productName, productType, productEAN, productQuantity, productShelfLife FROM warehouseinventory WHERE idDeliveredProduct = $DeliveredProductID";
+        $WarehouseResult = mysqli_query($conn, $WarehouseSQL);
+        $WarehouseRow = mysqli_fetch_assoc($WarehouseResult);
+        $companyName = $WarehouseRow["companyName"];
+        $productName = $WarehouseRow["productName"];
+        $productType = $WarehouseRow["productType"];
+        $productEAN = $WarehouseRow["productEAN"];
+        $productShelfLife = $WarehouseRow["productShelfLife"];
+        $OldQuantity = $WarehouseRow["productQuantity"];
+        $NewQuantity = $OldQuantity - $packageInput;
+        $UpdateWarehouseSQL = "UPDATE warehouseinventory SET productQuantity = $NewQuantity WHERE idDeliveredProduct = $DeliveredProductID";
+        mysqli_query($conn, $UpdateWarehouseSQL);
+
+        //上传包裹数据
+        $insertPackageSQL = "INSERT INTO packageproduct (idClient, companyName, productName, productType, productEAN, productQuantity, productShelfLife, pickupTime) VALUES ('$CookieClient', '$companyName', '$productName', $productType, '$productEAN', $packageInput, '$productShelfLife', '$CookieDate')";
+        if (mysqli_query($conn, $insertPackageSQL)) {
+            // 删除成功后重定向到当前页面，实现刷新效果
+            header("Location: " . $_SERVER["PHP_SELF"]);
+            exit();
+        } else {
+            echo "删除数据失败：" . mysqli_error($conn);
+        }
+    }
+}
 ?>
     <div class="header">
         <a href="./Index.html">👈Back to menu</a>
-        <p>Packing for&ensp;</p>
-        <select name="clientSelect" id="clientSelect" onchange="displayUserData()" >
-            <option value="none" selected disabled hidden>Please chose an user</option>
-            <?php
-            mysqli_data_seek($ClientResult, 0); // 重新定位结果集的指针到开头
-            while ($PTrow = mysqli_fetch_assoc($ClientResult)) {
-                echo '<option value="' . $PTrow["idClient"] . '">' . $PTrow["contactName"] . '</option>';
-            }
-            ?>
-        </select>
-        <p>&ensp;for&ensp;</p>
-        <input type="date" id="friday-date" min="<?= date('Y-m-d'); ?>" onchange="validateFridayDate()">
-        <p>&ensp;(Only Friday!)&ensp;</p>
-        <p id="userDetails">&ensp;</p>
+        <?php if($_COOKIE["PackageClient"] || $_COOKIE["PackageDate"]){ ?>
+        <div class="header">
+            <p>You are now packing for&ensp;</p>
+            <p>
+                <?php
+                mysqli_data_seek($ClientResult, 0); // 重新定位结果集的指针到开头
+                while ($PTrow = mysqli_fetch_assoc($ClientResult)) {
+                    if ($PTrow["idClient"] == $_COOKIE["PackageClient"]) {
+                        echo $PTrow["contactName"];
+                        echo (" for ");
+                        echo $_COOKIE["PackageDate"];
+                        echo (". His/Her family have ");
+                        echo $PTrow["adultsNumber"];
+                        echo (" adults, ");
+                        echo $PTrow["kidsNumber"];
+                        echo (" Kids and ");
+                        echo $PTrow["babyNumber"];
+                        echo (" baby.&ensp;");
+                    }
+                }
+                ?>
+            </p>
+            <button onclick="CleanUser()">Change client!</button>
+        </div>
+        <?php }else{ ?>
+        <div class="header">
+            <p>Packing for&ensp;</p>
+            <select name="clientSelect" id="clientSelect">
+                <option value="none" selected disabled hidden>Please chose an user</option>
+                <?php
+                mysqli_data_seek($ClientResult, 0); // 重新定位结果集的指针到开头
+                while ($PTrow = mysqli_fetch_assoc($ClientResult)) {
+                    echo '<option value="' . $PTrow["idClient"] . '">' . $PTrow["contactName"] . '</option>';
+                }
+                ?>
+            </select>
+            <p>&ensp;for&ensp;</p>
+            <input type="date" id="friday-date" min="<?= date('Y-m-d'); ?>" onchange="validateFridayDate()">
+            <p>&ensp;(Only Friday!)&ensp;</p>
+            <button onclick="ChooseUser()">Package!</button>
+        </div>
+        <?php } ?>
     </div>
-    <div class="inhoud">
+    <?php if($_COOKIE["PackageClient"] || $_COOKIE["PackageDate"]){ ?>
+    <div class="inhoud" >
         <div class="container-left">
             <table>
                 <tr>
@@ -123,14 +192,14 @@
                         } ?></td>
                         <td><?= $row["productEAN"]; ?></td>
                         <td><?= $row["productQuantity"]; ?></td>
-                        <td><input type="text" class="packageInput"></td>
-                        <td><?= $row["productShelfLife"]; ?></td>
-                        <td style="width:10%">
-                            <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
-                                <input type="hidden" name="packageID" value="<?= $row["idDeliveredProduct"] ?>">
-                                <button type="submit" >Package</button>
-                            </form>
-                        </td>
+                        <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+                            <td><input type="text" name="packageInput" class="packageInput"></td>
+                            <td><?= $row["productShelfLife"]; ?></td>
+                            <td style="width:10%">
+                            <input type="hidden" name="packageID" value="<?= $row["idDeliveredProduct"] ?>">
+                            <button type="submit" >Package</button>
+                            </td>
+                        </form>
                     </tr>
                 <?php ;} ?>
             </table>
@@ -153,33 +222,21 @@
                         <td><?php 
                         mysqli_data_seek($productTypeResult, 0); // 重新定位结果集的指针到开头
                         while($PTrow = mysqli_fetch_assoc($productTypeResult)) {
-                            if ($PTrow["idProductType"] == $row["productType"]) {
+                            if ($PTrow["idProductType"] == $row["ProductType"]) {
                                 echo $PTrow["productTypeName"];
                             }
                         } ?></td>
                         <td><?= $row["productEAN"]; ?></td>
                         <td><?= $row["productQuantity"]; ?></td>
-                        <td><input type="text" class="packageInput"></td>
                         <td><?= $row["productShelfLife"]; ?></td>
                     </tr>
                 <?php ;} ?>
             </table>
         </div>
     </div>
+    <?php } ?>
 </body>
 <script>
-function displayUserData() {
-    var select = document.getElementById("clientSelect");
-    var selectedClientId = select.value
-    <?php
-    mysqli_data_seek($ClientResult, 0); // 重新定位结果集的指针到开头
-    while ($PTrow = mysqli_fetch_assoc($ClientResult)) {
-        echo 'if (selectedClientId === "' . $PTrow["idClient"] . '") {';
-        echo 'document.getElementById("userDetails").innerHTML = "His/Her family have ' . $PTrow["adultsNumber"] . ' adult, ' . $PTrow["kidsNumber"] . ' children and ' . $PTrow["babyNumber"] . ' baby";';
-        echo '}';
-    }
-    ?>
-}
 function validateFridayDate() {
     var inputDate = document.getElementById("friday-date").value;
     var date = new Date(inputDate);
@@ -191,6 +248,23 @@ function validateFridayDate() {
       alert("You can only chose Friday!!");
       document.getElementById("friday-date").value = "";
     }
+}
+function ChooseUser(){
+    var client = document.getElementById("clientSelect").value;
+    var date = document.getElementById("friday-date").value;
+    if(client != "none" && date){
+        Cookies.set('PackageDate', date, { expires: 1 });
+        Cookies.set('PackageClient', client, { expires: 1 });
+        location.reload();
+    }
+}
+function CleanUser(){
+    Cookies.set('PackageDate', "0" , { expires: 1 });
+    Cookies.set('PackageClient', "0" , { expires: 1 });
+    location.reload();
+}
+function Package(){
+
 }
 </script>
 </html>
